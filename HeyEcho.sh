5da62cc880499455e4ee095d37a740874f2eeb49
NireSumika <NireSumika>
2020-03-24 21:13:54 +0800

    commit

---
+++ HeyEcho.sh
@@ -0,0 +1,1 @@
+nohup python3.5 /home/echo/Echo/HeyEcho.py /home/echo/Echo/models/HeyEcho.pmdl &
---
+++ hello.py
@@ -0,0 +1,6 @@
+import snowboydecoder
+import sys
+import signal
+def hellow():
+    snowboydecoder.play_audio_file()
+    print('Hey!')
